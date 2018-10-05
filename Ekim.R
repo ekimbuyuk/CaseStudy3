39306898002ea09470aa008fc63e48f9c1c75d2c
Ekim Buyuk <ekim.buyuk@duke.edu>
2018-10-04 21:16:41 -0400

    bihan knows

--- Ekim.R
+++ Ekim.R
@@ -1,8 +1,62 @@
 #Ekim's File
 
+#Clean Death Data
 new_data = merged_data %>%
-  group_by(COUNTY,race,hisp) %>%
+  filter(year == 2015) %>%
+  group_by(cores,race,hisp) %>%
   count()
+
+#Clean Birth Data
+Yr1116Birth = read_csv("Yr1116Birth.csv")
+birth_data = Yr1116Birth %>%
+  filter(YOB == 2015)
+
+birth_data$MRACER[birth_data$MRACER == 0] = 4
+birth_data$MRACER[birth_data$MRACER == 1] = 1
+birth_data$MRACER[birth_data$MRACER == 2] = 2
+birth_data$MRACER[birth_data$MRACER == 3] = 3
+birth_data$MRACER[birth_data$MRACER == 4] = 4
+birth_data$MRACER[birth_data$MRACER == 5] = 4
+birth_data$MRACER[birth_data$MRACER == 6] = 4
+birth_data$MRACER[birth_data$MRACER == 7] = 4
+birth_data$MRACER[birth_data$MRACER == 8] = 4
+
+birth_data = birth_data %>%
+  group_by(CORES, MRACER, MHISP) %>%
+  count()
+
+birth_data$MRACER[birth_data$MRACER == 0] = "Other"
+birth_data$MRACER[birth_data$MRACER == 1] = "White"
+birth_data$MRACER[birth_data$MRACER == 2] = "Black or African American"
+birth_data$MRACER[birth_data$MRACER == 3] = "American Indian or Alaska Native"
+birth_data$MRACER[birth_data$MRACER == 4] = "Other"
+birth_data$MRACER[birth_data$MRACER == 5] = "Other"
+birth_data$MRACER[birth_data$MRACER == 6] = "Other"
+birth_data$MRACER[birth_data$MRACER == 7] = "Other"
+birth_data$MRACER[birth_data$MRACER == 8] = "Other"
+
+birth_data$MHISP[birth_data$MHISP == "C"] = "Cuban"
+birth_data$MHISP[birth_data$MHISP == "M"] = "Mexican"
+birth_data$MHISP[birth_data$MHISP == "N"] = "Non-Hispanic"
+birth_data$MHISP[birth_data$MHISP == "O"] = "Other Hispanic"
+birth_data$MHISP[birth_data$MHISP == "P"] = "Puerto Rican"
+birth_data$MHISP[birth_data$MHISP == "S"] = "Central/South American"
+birth_data$MHISP[birth_data$MHISP == "U"] = "Unknown"
+
+colnames(new_data)[colnames(new_data) == "n"] <- "Total_Deaths"
+
+colnames(birth_data)[colnames(birth_data) == "CORES"] <- "cores"
+colnames(birth_data)[colnames(birth_data) == "MRACER"] <- "race"
+colnames(birth_data)[colnames(birth_data) == "MHISP"] <- "hisp"
+
+birth_deaths <- left_join(birth_data, new_data, by = c("CORES" = "cores", "MRACER" = "race", "MHISP" = "hisp"))
+
+birth_deaths <- merge(birth_data, new_data, by = c("cores", "race","hisp"), all.x=TRUE)
+
+birth_deaths <- left_join(birth_data, new_data, by = c("cores", "race","hisp"))
+
+birth_deaths$Total_Deaths[is.na(birth_deaths$Total_Deaths)] = 0
+birth_deaths$rate = birth_deaths$Total_Deaths/birth_deaths$n 
 
 new_data$log_totals = log(new_data$n)
 
